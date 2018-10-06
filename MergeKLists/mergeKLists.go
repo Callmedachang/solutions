58e610f9e22a561e7552d1e0de4cbce34cbf9233
芦文超 <bighandsome@luwenchao-2.local>
2018-10-06 11:15:29 +0800

    d

---
+++ MergeKLists/mergeKLists.go
@@ -0,0 +1,59 @@
+package main
+
+import "fmt"
+
+/**
+ * Definition for singly-linked list.*/
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeKLists(lists []*ListNode) *ListNode {
+	listsNUms := make([]int, 0)
+	res := &ListNode{}
+	pic := res
+	resLen := 0
+	lenTemp := lists
+	len := 0
+	for _, v := range lenTemp {
+		for v != nil {
+			len++
+			v = v.Next
+		}
+	}
+	for resLen < len {
+		tempMin := 99999999999
+		tempIndex := 0
+		for i, v := range lists {
+			if v != nil {
+				if v.Val < tempMin {
+					tempMin = v.Val
+					tempIndex = i
+				}
+
+			}
+		}
+		listsNUms = append(listsNUms, tempMin)
+		resLen++
+		lists[tempIndex] = lists[tempIndex].Next
+	}
+	for i, v := range listsNUms {
+		if i == 0 {
+			pic.Val = v
+		} else {
+			pic.Next = &ListNode{Val: v, Next: nil}
+			pic = pic.Next
+		}
+	}
+	return res
+}
+func main() {
+	l1 := &ListNode{1, &ListNode{2, &ListNode{3, &ListNode{Val: 3, Next: &ListNode{Val: 4}}}}}
+	l2 := &ListNode{5, &ListNode{6, &ListNode{7, nil}}}
+	l3 := &ListNode{7, &ListNode{8, &ListNode{9, nil}}}
+	in := make([]*ListNode, 0)
+	in = append(in, l1, l2, l3)
+	ss := mergeKLists(in)
+	fmt.Println(ss)
+}
