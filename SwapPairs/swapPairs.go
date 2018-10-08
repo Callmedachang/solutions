20d3c5b032eba9bdb212c12e536873c3b1aae5b8
芦文超 <alex@luwenchaodeMBP.lan>
2018-10-08 20:37:56 +0800

    add

---
+++ SwapPairs/swapPairs.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+/**
+给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+
+示例:
+
+给定 1->2->3->4, 你应该返回 2->1->4->3.
+说明:
+
+你的算法只能使用常数的额外空间。
+你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func swapPairs(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+
+	newHead := head.Next
+	head.Next = swapPairs(newHead.Next)
+	newHead.Next = head
+
+	return newHead
+}
+func main() {
+	l1 := &ListNode{1, &ListNode{2, &ListNode{3, &ListNode{4, nil}}}}
+	l2 := swapPairs(l1)
+	fmt.Println(l2)
+}
